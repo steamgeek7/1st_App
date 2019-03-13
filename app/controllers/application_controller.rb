28f5110a66927fe37400c7bd5f06f69915701d7a
Jefferson <steamgeek7@gmail.com>
2019-03-13 17:13:13 -0600

    5.3 emails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  ActionMailer::Base.mail(from: 'from@domain.com', to: 'to@domain.com', subject: "Welcome to My Awesome Site", body: 'I am the email body.').deliver_now
+    def thank_you
+      @name = params[:name]
+      @email = params[:email]
+      @message = params[:message]
+      ActionMailer::Base.mail(from: @email,
+      to: 'your-email@example.com',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+    end
 end
