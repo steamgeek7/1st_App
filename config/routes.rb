3917df6331dd0a40c0c8e2268d4be4a9bbf4d67a
Jefferson <steamgeek7@gmail.com>
2019-03-13 17:16:14 -0600

    5.3 emails

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   root 'simple_pages#landing_page'
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'simple_pages/thank_you'
 end
