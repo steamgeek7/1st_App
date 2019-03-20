f8e8e0d19eeaeaeb028bc056e6c5cce96e3cde6c
steamgeek7 <steamgeek7@gmail.com>
2019-03-20 15:21:47 -0600

    5.5 authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,7 @@ class ApplicationController < ActionController::Base
       subject: "A new contact form message from #{@name}",
       body: @message).deliver_now
     end
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_url, alert: exception.message
+  end
 end
