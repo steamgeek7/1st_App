b3fd854d81c961c6d4874152609a6f41b135e50d
steamgeek7 <steamgeek7@gmail.com>
2019-03-25 15:29:15 -0600

    5.7 comments) exit

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ApplicationRecord
   def self.search(search_term)
     Product.where("name LIKE ?", "%#{search_term}%")
   end
+  has_many :comments
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
 end
