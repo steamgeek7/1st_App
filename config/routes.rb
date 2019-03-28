1f8648921c5c94250d4a4433109d9ae0e163febb
steamgeek7 <steamgeek7@gmail.com>
2019-03-28 16:04:14 -0600

    5.8 Comments

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'simple_pages/thank_you'
+  resources :products do
+    resources :comments
+  end
 end
