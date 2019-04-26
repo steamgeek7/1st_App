85ed05c8f54f2d9694c4dc5a9b7be1672b186b87
Jefferson <steamgeek7@gmail.com>
2019-04-26 14:52:13 -0600

    6.5 revs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
-  ActionMailer::Base.mail(from: 'from@domain.com', to: 'to@domain.com', subject: "Welcome to My Awesome Site", body: 'I am the email body.').deliver_now
-    def thank_you
-      @name = params[:name]
-      @email = params[:email]
-      @message = params[:message]
-      ActionMailer::Base.mail(from: @email,
-      to: 'your-email@example.com',
-      subject: "A new contact form message from #{@name}",
-      body: @message).deliver_now
-    end
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+                            to: 'your-email@example.com',
+                            subject: "A new contact form message from #{@name}",
+                            body: @message).deliver_now
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, alert: exception.message
   end
