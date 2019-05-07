10a06cf7f97a53e4daf5d41859834ef9a6d2a73b
Richard Macias <rmacias@cloudflare.com>
2019-05-07 09:58:29 -0500

    fixes for stripe

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,30 +1,34 @@
 class PaymentsController < ApplicationController
   def create
     @token = params[:stripeToken]
-    @product = params[:product_id]
+    @product = Product.find(params[:product_id])
     @user = current_user
-  # Create the charge on Stripe's servers - this will charge the user's card
-  begin
-    charge = Stripe::Charge.create(
-      amount: 5000, # amount in cents, again
-      currency: "usd",
-      source: token,
-      description: params[:stripeEmail]
+    # Create the charge on Stripe's servers - this will charge the user's card
+    begin
+      charge = Stripe::Charge.create(
+        amount: 5000, # amount in cents, again
+        currency: "usd",
+        source: @token,
+        description: params[:stripeEmail]
       )
-    if charge.paid
-         Order.create(
-           user: @user,
-           product: @product,
-           total: @product.price
-         )
+      if charge.paid
+        Order.create(
+          user: @user,
+          product: @product,
+          total: @product.price
+        )
+      end
+    rescue Stripe::CardError => e
+      # The card has been declined
+      body = e.json_body
+      err = body[:error]
+      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
     end
-  rescue Stripe::CardError => e
-    # The card has been declined
-    body = e.json_body
-    err = body[:error]
-    flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+
+    redirect_to payments_thank_you_path(product_id: @product.id)
   end
 
-    redirect_to product_path(@product)
+  def thank_you
+    @product = Product.find(params[:product_id])
   end
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,13 +1,6 @@
-if Rails.env.production?
-  Rails.configuration.stripe = {
-    publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-    secret_key: ENV['STRIPE_SECRET_KEY']
-  }
-else
-  Rails.configuration.stripe = {
-    publishable_key: 'pk_test_zkT...',
-    secret_key: 'sk_test_I0e...'
-  }
-end
+Rails.configuration.stripe = {
+  publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+  secret_key: ENV['STRIPE_SECRET_KEY']
+}
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
