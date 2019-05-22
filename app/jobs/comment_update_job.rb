631d3d14ff57de80a49ca5483a13484bc0f97732
steamgeek7 <steamgeek7@gmail.com>
2019-05-22 15:10:59 -0600

    6.14

--- app/jobs/comment_update_job.rb
+++ app/jobs/comment_update_job.rb
@@ -5,4 +5,9 @@ class CommentUpdateJob < ApplicationJob
     # Do something later
      ProductChannel.broadcast_to(@product.id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: @comment, current_user: current_user }), average_rating: @product.average_rating)
   end
+  private
+
+  def render_comment(comment, current_user)
+    CommentsController.render(partial: 'comments/comment', locals: { comment: comment, current_user: current_user })
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,5 @@ validates :rating, numericality: { only_integer: true }
   belongs_to :user
   belongs_to :product
   scope :rating_desc, -> { order(rating: :desc) }
+after_create_commit { CommentUpdateJob.perform_later(self, self.user) }
 end
