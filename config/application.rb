27811d798edf86bdfc0cc9d2d3340cf1bcd7cd79
steamgeek7 <steamgeek7@gmail.com>
2019-05-22 19:40:29 -0600

    6.14

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-
+config.assets.initialize_on_precompile = false
 # require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
